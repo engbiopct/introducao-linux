f689ccae2219fa16f739a258ba3038a36f3fd9ea
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-01-12 16:30:36 -0300

    Create instaladores.sh

---
+++ comandos/instaladores.sh
@@ -0,0 +1,3 @@
+dpkg -i deb #instala um pacote DEB (distros Debian) (substituir a variável pacote.deb pelo nome do pacote de programa)
+
+rpm -Uvh rpm #instala um pacote RPM (Distros que utilizam RPM) (substituir a variávelpacote.rpmpelo nome do pacote de programa)
