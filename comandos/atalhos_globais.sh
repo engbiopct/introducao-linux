ba9e4547d6b2e44b3279262dd76ee2463c7b7741
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-01-12 16:19:25 -0300

    Update atalhos_globais.sh

--- comandos/atalhos_globais.sh
+++ comandos/atalhos_globais.sh
@@ -1,13 +1,13 @@
-Ctrl+C //(cancela o comando atual em funcionamento)
+Ctrl+C # cancela o comando atual em funcionamento
 
-Ctrl+Z //(para o comando atual, retorna com fg em primeiro plano Linux ou bg em segundo plano)
+Ctrl+Z # para o comando atual, retorna com fg em primeiro plano Linux ou bg em segundo plano
 
-Ctrl+D //(faz logout da sessão atual; similar ao comando exit)
+Ctrl+D # faz logout da sessão atual; similar ao comando exit
 
-Ctrl+W //(apaga uma palavra na linha atual)
+Ctrl+W # apaga uma palavra na linha atual
 
-Ctrl+U //(apaga a linha inteira)
+Ctrl+U # apaga a linha inteira
 
-Ctrl+R //(tecle para mostrar um comando recente)
+Ctrl+R # tecle para mostrar um comando recente
 
-!! //(repete o último comando)
+!! # repete o último comando
